bfb857f8ba3f089182513cd038c05e5135ea42d2
Alex Nikiforov <nikiforov.pub@gmail.com>
2011-11-28 15:33:15 +0400

    kernel: arm: fix init script

--- kernel/arm/qemu/qemu_launch_initrd.sh
+++ kernel/arm/qemu/qemu_launch_initrd.sh
@@ -1,3 +1,3 @@
-qemu-system-arm -initrd ./rootfs.ext2 -kernel ./zImage -append "console=ttyAMA0 root=/dev/ram rw" -serial stdio -M realview-pbx-a9 -m 1024 -cpu cortex-a9 -smp 2 -redir tcp:2222::22 -redir tcp:1234::1234
+qemu-system-arm -initrd ./rootfs -kernel ./zImage -append "console=ttyAMA0 root=/dev/ram rw" -serial stdio -M realview-pbx-a9 -m 1024 -cpu cortex-a9 -smp 2 -redir tcp:2222::22 -redir tcp:1234::1234
 #gdb usage -redir tcp:1234::1234
 #ssh, scp usage -redir tcp:2222::22
